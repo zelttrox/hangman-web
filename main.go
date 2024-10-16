c904dcdfccebff69b226c70f1a0471fbf35d4942
zelttrox <dev.zeltrox@gmail.com>
2024-10-16 14:00:23 +0200

    Create website function

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import hg "hangman/scripts"
+import (
+	"fmt"
+	hg "hangman/scripts"
+	web "http/web"
+)
 
 func main() {
+	web.CreateWebsite()
+	fmt.Println("Website launched")
 	hg.Init() // Initialisation du jeu
 	hg.Run()  // Lancement de la boucle du jeu
 }
---
+++ web/net.go
@@ -0,0 +1,7 @@
+package http
+
+import "net/http"
+
+func CreateWebsite() {
+	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+}
