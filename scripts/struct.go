d13fd819f681a5f67a30419eb4e489d587c9eefa
CharLuney <charleen.beheray@ynov.com>
2024-11-09 19:15:24 +0100

    Cleanup + New functions

--- scripts/struct.go
+++ scripts/struct.go
@@ -1,20 +1,25 @@
 package hangman
 
-var Dictionary string        // Dictionnaire de mots choisi
-var WordList []string        // Dictionnaire des mots
-var Word string              // Mot choisi au hasard *
-var Blankspace []string      // Nombre de cases du Hangman *
-var LetterAmount int         // Nombre de lettres a reveler
+var Dictionary string     // Dictionnaire de mots choisi
+var WordList []string     // Dictionnaire des mots
+var WordChosen string     //
+var Word string           // Mot choisi au hasard *
+var WordProgress []string // Nombre de cases du Hangman *
+var LetterAmount int      // Nombre de lettres a reveler
 
-var HangmanPosition []string // Liste de positions du Hangmans
-var HangmanProgress int      // Progression du Hangman
+var HangmanPosition []string // Liste de positions du Hangman *
+var HangmanProgress string   // Position du Hangman
 var LinesToAdd int           // Lignes du Hangman à afficher
 var HangmanLen int           // Taille d'une frame du Hangman
 
-var Attempts int              // Nombre d'essais restants
+var Attempts int              // Nombre d'essais restants *
 var MaxAttempts int           // Nombre total d'essais
 var AttemptedLetters []string // Lettres déjà essayées *
-var LetterTried bool          // Est vrai si la lettre entrée a déjà été testée
+var LetterTried bool          // Est vrai si la lettre entrée a déjà été 
+
+var MainPage string
+var WinPage string
+var LosePage string
 
 var IsHardcoreMode bool // Défini si le jeu est en mode hardcore ou en normal
 
